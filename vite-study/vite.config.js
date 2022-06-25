4f9f9deda98ffef6e8ade584b81c234cba514f11
XZ361 <2457307263@qq.com>
2022-06-25 21:30:28 +0800

    添加alias别名

--- vite-study/vite.config.js
+++ vite-study/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias:({
+    "@": path.resolve(__dirname, "src"),
+    "comps": path.resolve(__dirname, "src/components"),
+    "views": path.resolve(__dirname, "src/views"),
+    "routes": path.resolve(__dirname, "src/routes"),
+    "apis": path.resolve(__dirname, "src/apis"),
+    "utils": path.resolve(__dirname, "src/utils"),
+    "styles": path.resolve(__dirname, "src/styles")
+  }),
   plugins: [vue()]
 })
