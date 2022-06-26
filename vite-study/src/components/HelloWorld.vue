d89b73331977a9d1a6189a287f8b04bbae15423f
XZ361 <2457307263@qq.com>
2022-06-26 18:00:55 +0800

    整合样式管理

--- vite-study/src/components/HelloWorld.vue
+++ vite-study/src/components/HelloWorld.vue
@@ -55,7 +55,5 @@ fetch('/api/getUsers')
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
+
 </style>
--- vite-study/src/main.js
+++ vite-study/src/main.js
@@ -1,6 +1,10 @@
 import { createApp, useAttrs } from 'vue'
 import App from './App.vue'
+// 路由
 import router from './router'
+// 状态
 import store from './store'
+// 全局样式
+import './styles/index.scss'
 
 createApp(App).use(router).use(store).mount('#app')
